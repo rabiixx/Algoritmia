4de062227d9e788e74e0522c0f50cf4c072f8385
rabiixx <rabiixx@gmail.com>
2019-09-16 19:57:26 +0200

    p1_ejer2 mod

--- Algoritmia 2019-2020/Material Practicas/p1_ejer3.c
+++ Algoritmia 2019-2020/Material Practicas/p1_ejer3.c
@@ -1,11 +1,8 @@
-
-
-
-typedef struct celda {
+typedef struct datos {
 	int fechaD;
 	int numF;
 	int hack;
-}celda;
+}datosD;
 
 
 typedef struct _cola {
@@ -16,8 +13,54 @@ typedef struct _cola {
 
 
 
-int int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-	
+	cola *c = NULL;
+	int numDesastres;
+
+	printf("Introduce numero de desastres: \n");
+	scanf("%d", &numDesastres);
+
+	datosD *arrayD = (datosD*)calloc(numDesastres, sizeof(datosD));
+
+	for (int i = 0; i < ; ++i) {
+		printf("Introduce fecha y victimas: \n");
+		scanf("%d %d", &datosD[i].fecha, &datosD[i].numF);
+	}
+
+	if (arrayD[i-1].numF > arrayD[i].numF) {
+		arrayD[i].hack = arrayD[i-1].fechaD;
+	} else {
+		
+	}
+
+
+
 	return 0;
+}
+
+void piruleta(cola *c) {
+
+	c->head->hack = 0;
+	c->head->sig->hack = 0;
+	int maxVict = INT_MIN;
+
+	celda->aux
+
+	if()
+
+
+
+}
+
+
+void encolar(cola *c, celda *aux) {
+
+	if (esNulaCola(c)) { 
+		c->head = aux;
+		c->last = aux;
+	} else {
+		(c->last).sig = aux;
+		c->last = aux;
+	}
 }
