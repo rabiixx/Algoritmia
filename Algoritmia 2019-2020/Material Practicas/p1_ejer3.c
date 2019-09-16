08c6ad5ed27e9772947603f8100968c9286fb457
rabiixx <rabiixx@gmail.com>
2019-09-16 18:45:45 +0200

    res

---
+++ Algoritmia 2019-2020/Material Practicas/p1_ejer3.c
@@ -0,0 +1,23 @@
+
+
+
+typedef struct celda {
+	int fechaD;
+	int numF;
+	int hack;
+}celda;
+
+
+typedef struct _cola {
+	int numDesastres;
+	celda *head;
+	celda *last;
+}cola;
+
+
+
+int int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
