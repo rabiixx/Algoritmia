c18b2bc1108f58671f7e3ffad8cf65463791e157
rabiixx <rabiixx@gmail.com>
2019-09-18 18:43:10 +0200

    colaEnteros.c bis terminada

---
+++ Algoritmia 2019-2020/Material Practicas/p1_ejer2_sol_bis/colaEnteros.c
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "colaEnteros.h"
+
+
+void nuevaCola(tipoCola *c) {
+	c->ini = NULL;
+	c->fin = NULL;
+}
+
+void encolar(tipoCola *c, tipoElementoCola auxElem) {
+
+	celdaCola *aux = (celdaCola*)malloc(sizeof(celdaCola));
+	aux->elem = auxElem;
+	aux->sig = NULL;
+
+	if (esNulaCola(*c)) {
+		c->ini = aux;
+		c->fin = aux;
+	} else {
+		(c->fin)->sig = aux;
+		c->fin = aux;
+	}
+
+}
+
+void desencolar(tipoCola *c) {
+	if (esNulaCola(*c)) {
+		printf("[+] La cola esta vacia.\n");
+	} else {
+		celdaCola *aux;
+		aux = c->ini;
+		c->ini = aux->sig;		//c->head = (c->head)->sig
+		printf("[+] %d desencolado.\n", aux->elem);
+		free(aux);
+	}
+}
+
+tipoElementoCola frente(tipoCola c) {
+	if (esNulaCola(c))
+		printf("No se puede obtener el frente en una cola vacia.\n");
+	else
+		return (c.ini)->elem;
+}
+
+bool esNulaCola(tipoCola c) {
+	return ( (c.ini == NULL) && (c.fin == NULL) );
+}
