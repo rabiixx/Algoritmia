568db187b646a92aab38fa3df206eb4ad938f978
rabiixx12 <99rubenche@gmail.com>
2019-10-08 12:35:39 +0200

    update p2_ejer7_sol

--- Algoritmia 2019-2020/practica2/p2_ejer7_sol/p2_ejer7_sol.c
+++ Algoritmia 2019-2020/practica2/p2_ejer7_sol/p2_ejer7_sol.c
@@ -1,48 +1,47 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "arbolBin.h"
 
-typedef int tipoElementoArbolBin;
-typedef struct celdaABin{
-	 tipoElementoArbolBin elem;
-	 struct celdaABin *izda;
-	 struct celdaABin *dcha;
- }celdaArbolBin; 
-typedef celdaArbolBin *tipoArbolBin;
-
-
-
-
+tipoArbolBin abeto_recursivo(int arrPre[], int arrIn[], int low_index, int high_index, int *preIndex);
+int searchIndex(tipoElementoArbolBin in[], int low, int high, int elem );
 
 int main(int argc, char const *argv[]) {
 	
 	unsigned int N;
 	printf("Introduce el numero de elementos: ");
-	scanf("%d", &N);
+	scanf("%u", &N);
 
 	int arrPre[N];
 	int arrIn[N];
 
 	printf("Introduce Preorden: ");
 	for (int i = 0; i < N; ++i){
-		scanf("%d", &arrPre[i])
+		scanf("%d", &arrPre[i]);
 	}
 	
 	printf("Introduce Inorden: ");
 	for (int i = 0; i < N; ++i){
-		scanf("%d", &arrIn[i])
+		scanf("%d", &arrIn[i]);
 	}
 
 
+	tipoArbolBin a;
+	nuevoArbolBin(&a);
+
 	int preIndex = 0;	/* Guarda el indice del array en Preorden */
-	abeto_recursivo(arrPre, arrIn, 0, N - 1, &preIndex);
+	a = abeto_recursivo(arrPre, arrIn, 0, N - 1, &preIndex);
+
+	printf("El arbol en postorden es: ");
+	postorden(a);
+	printf("\n");
 
 	return 0;
 }
 
 
 
-tipoArbolBin abeto_recursivo(arrPre[], arrIn[], int low_index, int high_index, int *preIndex) {
+tipoArbolBin abeto_recursivo(int arrPre[], int arrIn[], int low_index, int high_index, int *preIndex) {
 
 	/* Caso1: No tiene hijo derecho o hijo izquierdo */
 	if (low_index > high_index) 
@@ -53,14 +52,14 @@ tipoArbolBin abeto_recursivo(arrPre[], arrIn[], int low_index, int high_index, i
 
 	/* El nodo padre no tiene ningun hijo */
 	if (low_index == high_index )
-		return construir(arrPre[preIndex++], NULL, NULL);
+		return construir(arrPre[(*preIndex)++], NULL, NULL);
 
-	index = search(arrIn, low_index, high_index, arrPre[low_index]);
+	int index = searchIndex(arrIn, low_index, high_index, arrPre[(*preIndex)]);
 
-	tipoArbolBin aux1 =	abeto_recursivo(arrPre, arrIn, low_index, index -1);
-	tipoArbolBin aux2 = abeto_recursivo(arrPre, arrIn, low_index, index + 1, high_index);
+	tipoArbolBin aux1 =	abeto_recursivo(arrPre, arrIn, low_index, index -1, preIndex);
+	tipoArbolBin aux2 = abeto_recursivo(arrPre, arrIn, index + 1, high_index, preIndex);
 
-	return construir(arrPre[preIndex++], aux1, aux2);
+	return construir(arrPre[(*preIndex)++], aux1, aux2);
 
 }
 
